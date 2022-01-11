c1751bcee784e8457160756926e006ca3b6bcece
Peter De Genaro <pdegenaro@gmail.com>
2022-01-11 18:24:35 -0500

    add guess param

--- src/commands/wordle.js
+++ src/commands/wordle.js
@@ -6,8 +6,10 @@ const fullUniqueList = Array.from(new Set([...wordlist.solutions, ...wordlist.va
 const wordle = {
   do: (args, message) => {
     const userWord = args[0];
+    let userGuesses = parseInt(args[1], 10);
+    if (userGuesses > 6) { userGuesses = 6; }
     const randomWord = wordlist.solutions[Math.floor(Math.random() * wordlist.solutions.length)];
-    const wg = new WordleGame(fullUniqueList, userWord || randomWord, 6);
+    const wg = new WordleGame(fullUniqueList, userWord || randomWord, userGuesses || 6);
     const botMessage = wg.startGuessing();
     message.reply(botMessage.join('\n'));
   },
