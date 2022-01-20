b8733202fce62364ce3ce88b7824d850f72ebafb
Dana Czinsky <danaczinsky@gmail.com>
2022-01-19 23:58:56 -0500

    address comments

--- src/scripts/wordle_guesser.js
+++ src/scripts/wordle_guesser.js
@@ -5,10 +5,10 @@ const removeFromList = (list, char) => list.filter((item) => item !== char);
 class WordleGame {
   constructor(dict, secret, maxGuesses = 100) {
     this.answer = Array.from({ length: 5 }, () => '-');
-    this.confirmedLetters = [];
+    this.confirmedLettersInSecret = [];
     this.dictionary = dict.map((word) => word.toUpperCase());
     this.maxGuesses = maxGuesses;
-    this.possibleLetters = Array.from({ length: 5 }, () => ALPHA);
+    this.possibleLettersAtPosition = Array.from({ length: 5 }, () => ALPHA);
     this.secret = secret.toUpperCase();
   }
 
@@ -21,15 +21,15 @@ class WordleGame {
     guess.split('').forEach((char, i) => {
       if (guess[i] === this.secret[i]) {
         this.answer[i] = char;
-        this.possibleLetters[i] = [char];
-        this.confirmedLetters.push(char);
+        this.possibleLettersAtPosition[i] = [char];
+        this.confirmedLettersInSecret.push(char);
       } else if (this.secret.includes(char)) {
-        const removeLetter = removeFromList(this.possibleLetters[i], char);
-        this.possibleLetters[i] = removeLetter;
-        this.confirmedLetters.push(char);
+        const removeLetterAtPosition = removeFromList(this.possibleLettersAtPosition[i], char);
+        this.possibleLettersAtPosition[i] = removeLetterAtPosition;
+        this.confirmedLettersInSecret.push(char);
       } else {
-        const newLetters = this.possibleLetters.map((list) => removeFromList(list, char));
-        this.possibleLetters = newLetters;
+        const newLetters = this.possibleLettersAtPosition.map((list) => removeFromList(list, char));
+        this.possibleLettersAtPosition = newLetters;
       }
     });
   }
@@ -40,8 +40,12 @@ class WordleGame {
   }
 
   isWordValid(word) {
-    return word.split('').every((char, i) => this.possibleLetters[i].includes(char))
-      && this.confirmedLetters.every((char) => word.includes(char));
+    const hasConfirmedLetters = this.confirmedLettersInSecret.every((char) => word.includes(char));
+
+    const isValidByPosition = word.split('')
+      .every((char, i) => this.possibleLettersAtPosition[i].includes(char));
+
+    return hasConfirmedLetters && isValidByPosition;
   }
 
   startGuessing() {
